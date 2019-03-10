a80795a05e5646800fbcaac75ec9182dcda260ea
Eather <eathertoo@gmail.com>
2019-03-10 15:29:21 +0800

    配置文件完全配置

--- MyBlog/src/main/java/com/blog/controller/IndexController.java
+++ MyBlog/src/main/java/com/blog/controller/IndexController.java
@@ -5,6 +5,7 @@ import com.blog.service.UserService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServlet;
@@ -19,12 +20,14 @@ public class IndexController extends HttpServlet{
     private static Logger logger = Logger.getLogger(IndexController.class);
 
     @RequestMapping(value = "/demo")
-    public String demo(){
+    public String demo(Model model){
         User user = new User();
         user.setUserNickname("Eve");
         logger.debug("插入新用户");
         userService.regist(user);
-        return "index";
+        String str = System.getProperty("webApp.root");
+        model.addAttribute("message", str);
+        return "demo/index";
     }
 
 }
